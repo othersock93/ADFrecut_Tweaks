f344ac2022c825811274916cb80ec1084f0cd272
Barais20 <52481962+Barais20@users.noreply.github.com>
2023-04-24 19:11:02 -0400

    More cleaning

---
+++ Addons/adfu_faction/config.cpp
@@ -0,0 +1,337 @@
+class CfgPatches
+{
+	class ADFU_faction
+	{
+		units[]=
+		{
+			"ADFU_Soldier_base_F",
+			"ADFU_OPFOR_Soldier_base_F",
+			"FLAG_ADFUNCUT_F",
+			"FLAG_ADFU_F",
+			"FLAG_ADFOPFOR_F"
+		};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"A3_Characters_F_BLUFOR"
+		};
+		author[]=
+		{
+			"Australian Defence Force: Uncut"
+		};
+		version="0.5.0";
+		versionStr="0.5.0";
+		versionAr[]={0,5,0};
+	};
+};
+class CfgUnitInsignia
+{
+	class ADFU_insignia_1_armoured
+	{
+		displayName="1st Armoured Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_1arm.paa";
+	};
+	class ADFU_insignia_1_aviation
+	{
+		displayName="1st Aviation Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_1av.paa";
+	};
+	class ADFU_insignia_1_cdo_patch
+	{
+		displayName="1st Commando Regiment (Patch)";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_1cdo.paa";
+	};
+	class ADFU_insignia_1_rar
+	{
+		displayName="1st Royal Australian Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_1rar.paa";
+	};
+	class ADFU_insignia_2_cavalry
+	{
+		displayName="2nd Cavalry Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_2cav.paa";
+	};
+	class ADFU_insignia_2_cdo_patch
+	{
+		displayName="2nd Commando Regiment (Patch)";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_2cdo.paa";
+	};
+	class ADFU_insignia_2_light_horse
+	{
+		displayName="2/14th Lighthorse Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_2light.paa";
+	};
+	class ADFU_insignia_2_rar
+	{
+		displayName="2nd Royal Australian Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_2rar.paa";
+	};
+	class ADFU_insignia_3_cavalry
+	{
+		displayName="3rd Cavalry Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_3cav.paa";
+	};
+	class ADFU_insignia_3_rar
+	{
+		displayName="3rd Royal Australian Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_3rar.paa";
+	};
+	class ADFU_insignia_5_aviation
+	{
+		displayName="5th Aviation Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_5av.paa";
+	};
+	class ADFU_insignia_5_rar
+	{
+		displayName="5th Royal Australian Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_5rar.paa";
+	};
+	class ADFU_insignia_6_aviation
+	{
+		displayName="6th Aviation Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_6av.paa";
+	};
+	class ADFU_insignia_6_rar
+	{
+		displayName="6th Royal Australian Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_6rar.paa";
+	};
+	class ADFU_insignia_7_rar
+	{
+		displayName="7th Royal Australian Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_7rar.paa";
+	};
+	class ADFU_insignia_aircrew
+	{
+		displayName="Aircrew Deployment Patch";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_aircrew.paa";
+	};
+	class ADFU_insignia_armour_hq
+	{
+		displayName="Armour Headquarters";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_armhq.paa";
+	};
+	class ADFU_insignia_aviation_hq
+	{
+		displayName="Aviation Headquarters";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_avhq.paa";
+	};
+	class ADFU_insignia_isaf
+	{
+		displayName="International Security Assistance Force";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_isaf.paa";
+	};
+	class ADFU_insignia_jocom
+	{
+		displayName="Joint Operations Command";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_jocom.paa";
+	};
+	class ADFU_insignia_medic
+	{
+		displayName="Combat Medical Attendant";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_medic.paa";
+	};
+	class ADFU_insignia_jtac
+	{
+		displayName="Joint Terminal Attack Controller";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_jtac.paa";
+	};
+	class ADFU_insignia_sasr_patch
+	{
+		displayName="Special Air Service Regiment (Patch)";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_sasr.paa";
+	};
+	class ADFU_insignia_soer_patch
+	{
+		displayName="Special Operation Engineer Regiment (Patch)";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_soer.paa";
+	};
+	class ADFU_insignia_1cdo_color
+	{
+		displayName="1st Commando Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_sotg1cdo.paa";
+	};
+	class ADFU_insignia_2cdo_color
+	{
+		displayName="2nd Commando Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_sotg2cdo.paa";
+	};
+	class ADFU_insignia_sotghq_color
+	{
+		displayName="Special Operation Task Group HQ";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_sotghq.paa";
+	};
+	class ADFU_insignia_sasr_color
+	{
+		displayName="Special Air Service Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_sotgsasr.paa";
+	};
+	class ADFU_insignia_soer_color
+	{
+		displayName="Special Operation Engineer Regiment";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_sotgsoer.paa";
+	};
+	class ADFU_insignia_sopt_color
+	{
+		displayName="Special Operations Parachute Training";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_sotgpara.paa";
+	};
+	class ADFU_insignia_sotraining_color
+	{
+		displayName="Special Operations Training Group";
+		author="$STR_ADFU_AUTHOR";
+		texture="ADFU_faction\insignia\ADFU_insig_sotgtraining.paa";
+	};
+};
+class cfgFactionClasses
+{
+	class ADFU
+	{
+		displayName="$STR_ADFU_faction_NAME";
+		flag="\ADFU_faction\data\ADFU_flag_co.paa";
+		icon="\ADFU_faction\data\ADFU_icon_co.paa";
+		priority=1;
+		side=1;
+	};
+	class ADFU_OPFOR
+	{
+		displayName="$STR_ADFU_OPFOR_FACTION_NAME";
+		flag="\ADFU_faction\data\adfopfor_flag_co.paa";
+		icon="\ADFU_faction\data\adfopfor_icon_co.paa";
+		side=0;
+		priority=1;
+	};
+};
+class CfgVehicleClasses
+{
+	class ADFU_MEN_DPCU
+	{
+		displayName="$STR_ADFU_MEN_DPCU";
+	};
+	class ADFU_MEN_DPDU
+	{
+		displayName="$STR_ADFU_MEN_DPDU";
+	};
+	class ADFU_MEN_MCAM
+	{
+		displayName="$STR_ADFU_MEN_MCAM";
+	};
+	class ADFU_MEN_AMP
+	{
+		displayName="$STR_ADFU_MEN_AMP";
+	};
+	class ADFU_MEN_AMCU
+	{
+		displayName="$STR_ADFU_MEN_AMCU";
+	};
+	class ADFU_MEN_RAAF
+	{
+		displayName="$STR_ADFU_MEN_RAAF";
+	};
+	class ADFU_MEN_RAN
+	{
+		displayName="$STR_ADFU_MEN_RAN";
+	};
+};
+class CfgVehicles
+{
+	class B_Soldier_base_F;
+	class ADFU_Soldier_base_F: B_Soldier_base_F
+	{
+		scope=2;
+		author="$STR_ADFU_AUTHOR";
+		_generalMacro="ADFU_Soldier_base_F";
+		side=1;
+		faction="ADFU";
+		vehicleClass="";
+	};
+	class ADFU_OPFOR_Soldier_base_F: B_Soldier_base_F
+	{
+		scope=2;
+		author="$STR_ADFU_AUTHOR";
+		_generalMacro="ADFOPFOR_Soldier_base_F";
+		side=0;
+		faction="ADFU_OPFOR";
+	};
+	class FLAG_NATO_F;
+	class FLAG_ADFUNCUT_F: FLAG_NATO_F
+	{
+		scope=2;
+		displayName="$STR_ADFU_MARKER_ADFUNCUT";
+		class eventHandlers
+		{
+			init="(_this select 0) setFlagTexture '\ADFU_faction\data\adfuncut_flag_co.paa'";
+		};
+	};
+	class FLAG_ADFU_F: FLAG_NATO_F
+	{
+		scope=2;
+		displayName="$STR_ADFU_MARKER_ADF";
+		class eventHandlers
+		{
+			init="(_this select 0) setFlagTexture '\ADFU_faction\data\ADFU_flag_co.paa'";
+		};
+	};
+	class FLAG_CSAT_F;
+	class FLAG_ADFOPFOR_F: FLAG_CSAT_F
+	{
+		scope=2;
+		displayName="$STR_ADFU_MARKER_ADFOPFOR";
+		class eventHandlers
+		{
+			init="(_this select 0) setFlagTexture '\ADFU_faction\data\adfopfor_flag_co.paa'";
+		};
+	};
+};
+class cfgMarkers
+{
+	class ADFU_flag
+	{
+		name="Australian Flag";
+		icon="\ADFU_faction\data\ADFU_marker_ca.paa";
+		color[]={1,1,1,1};
+		size=32;
+		shadow=0;
+		scope=2;
+	};
+	class ADFU_opfor_flag
+	{
+		name="Australian Colonial (Opfor)";
+		icon="\ADFU_faction\data\adfopfor_marker_ca.paa";
+		color[]={1,1,1,1};
+		size=32;
+		shadow=0;
+		scope=2;
+	};
+};
