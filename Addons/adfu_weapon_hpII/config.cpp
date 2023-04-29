8515ae2911c19acbf0fc4b0eb2009ed1e4dbcad6
IsoBones <isoboness@gmail.com>
2023-04-30 00:23:29 +1000

    re-write config.cpp
    Re-wrote config.cpp and included recoil in new config.
    Still needs reload sound added

--- Addons/adfu_weapon_hpII/config.cpp
+++ Addons/adfu_weapon_hpII/config.cpp
@@ -13,233 +13,140 @@ class CfgPatches
 			"A3_Weapons_F",
 			"A3_Weapons_F_Acc"
 		};
+		author[]=
+		{
+			"ADF Re-Cut | ADF: Uncut"
+		};
 	};
 };
-class Mode_SemiAuto;
 class MuzzleSlot;
+class CowsSlot;
+class PointerSlot;
+class UnderBarrelSlot;
+class ItemInfo;
+class Single;
 class CfgWeapons
 {
 	class Pistol_Base_F;
-	class ADFU_weapon_hpII: Pistol_Base_F
+	class ADFU_pistol_base: Pistol_Base_F
+	{
+		class Single;
+		
+	};
+	class ADFU_weapon_hpII: ADFU_pistol_base
 	{
 		scope=2;
-		displayname="Browning High Power MkII";
+		displayName="Browning High Power MkII";
 		author="$STR_ADFU_AUTHOR";
 		model="\ADFU_weapon_hpII\ADFU_PISTOL_HPII.p3d";
 		picture="\ADFU_weapon_hpII\UI\gear_ADFU_HPII_x_ca.paa";
 		UiPicture="\A3\weapons_f\data\UI\icon_regular_CA.paa";
+		class Library
+		{
+			libTextDesc="Browning High Power MkII";
+		};
+		cursor="";
 		magazines[]=
 		{
 			"16Rnd_9x21_Mag"
 		};
 		reloadAction="GestureReloadPistol";
-		dexterity=2;
-		bullet1[]=
-		{
-			"A3\sounds_f\weapons\shells\9mm\metal_9mm_01",
-			0.50118703,
-			1,
-			15
-		};
-		bullet2[]=
-		{
-			"A3\sounds_f\weapons\shells\9mm\metal_9mm_02",
-			0.50118703,
-			1,
-			15
-		};
-		bullet3[]=
-		{
-			"A3\sounds_f\weapons\shells\9mm\metal_9mm_03",
-			0.50118703,
-			1,
-			15
-		};
-		bullet4[]=
-		{
-			"A3\sounds_f\weapons\shells\9mm\metal_9mm_04",
-			0.50118703,
-			1,
-			15
-		};
-		bullet5[]=
-		{
-			"A3\sounds_f\weapons\shells\9mm\dirt_9mm_01",
-			0.39810699,
-			1,
-			15
-		};
-		bullet6[]=
-		{
-			"A3\sounds_f\weapons\shells\9mm\dirt_9mm_02",
-			0.39810699,
-			1,
-			15
-		};
-		bullet7[]=
-		{
-			"A3\sounds_f\weapons\shells\9mm\dirt_9mm_03",
-			0.39810699,
-			1,
-			15
-		};
-		bullet8[]=
-		{
-			"A3\sounds_f\weapons\shells\9mm\dirt_9mm_04",
-			0.39810699,
-			1,
-			15
-		};
-		bullet9[]=
-		{
-			"A3\sounds_f\weapons\shells\9mm\grass_9mm_01",
-			0.22387201,
-			1,
-			15
-		};
-		bullet10[]=
-		{
-			"A3\sounds_f\weapons\shells\9mm\grass_9mm_02",
-			0.22387201,
-			1,
-			15
-		};
-		bullet11[]=
-		{
-			"A3\sounds_f\weapons\shells\9mm\grass_9mm_03",
-			0.22387201,
-			1,
-			15
-		};
-		bullet12[]=
-		{
-			"A3\sounds_f\weapons\shells\9mm\grass_9mm_04",
-			0.22387201,
-			1,
-			15
-		};
-		soundBullet[]=
-		{
-			"bullet1",
-			0.082999997,
-			"bullet2",
-			0.082999997,
-			"bullet3",
-			0.082999997,
-			"bullet4",
-			0.082999997,
-			"bullet5",
-			0.082999997,
-			"bullet6",
-			0.082999997,
-			"bullet7",
-			0.082999997,
-			"bullet8",
-			0.082999997,
-			"bullet9",
-			0.082999997,
-			"bullet10",
-			0.082999997,
-			"bullet11",
-			0.082999997,
-			"bullet12",
-			0.082999997
-		};
-		sounds[]=
-		{
-			"StandardSound"
-		};
-		class BaseSoundModeType
-		{
-			weaponSoundEffect="DefaultRifle";
-			closure1[]=
-			{
-				"A3\Sounds_F\weapons\closure\closure_handgun_3",
-				1.0351422,
-				1,
-				30
-			};
-			closure2[]=
-			{
-				"A3\Sounds_F\weapons\closure\closure_handgun_3",
-				1.0351422,
-				1.1,
-				30
-			};
-			soundClosure[]=
-			{
-				"closure1",
-				0.5,
-				"closure2",
-				0.5
-			};
-		};
-		class StandardSound: BaseSoundModeType
-		{
-			begin1[]=
-			{
-				"ADFU_weapon_hpII\sound\ADFU_HPII_1",
-				1,
-				1.1,
-				700
-			};
-			begin2[]=
-			{
-				"ADFU_weapon_hpII\sound\ADFU_HPII_2",
-				1.15,
-				1.1,
-				700
-			};
-			begin3[]=
-			{
-				"ADFU_weapon_hpII\sound\ADFU_HPII_1",
-				1,
-				1.1,
-				700
-			};
-			soundBegin[]=
-			{
-				"begin1",
-				0.33000001,
-				"begin2",
-				0.33000001,
-				"begin3",
-				0.34
-			};
-		};
-		reloadtime=0.13;
-		dispersion=0.003;
-		drySound[]=
-		{
-			"A3\sounds_f\weapons\other\dry1",
-			0.39810699,
-			1,
-			20
-		};
-		reloadMagazineSound[]=
-		{
-			"A3\sounds_f\weapons\pistols\p07_reload",
-			0.56234097,
-			1,
-			30
-		};
-		recoil="recoil_pistol_light";
-		recoilProne="recoil_prone_pistol_light";
-		class Library
-		{
-			libTextDesc="9mm Browning High Power II";
-		};
-		descriptionShort="9mm Browning High Power II";
 		class WeaponSlotsInfo
 		{
-			mass=20;
-			class CowsSlot
+			mass=5;
+			class MuzzleSlot: MuzzleSlot
 			{
+				compatibleitems[]=
+				{};
+			};
+			class CowsSlot: CowsSlot
+			{};
+			class PointerSlot: PointerSlot
+			{
+				compatibleitems[]=
+				{};
+			};
+			class UnderBarrelSlot: UnderBarrelSlot
+			{
+				compatibleitems[]={};
 			};
 		};
-		class ItemInfo
+		modes[]=
 		{
-			priority=2;
+			"Single"
+		};
+		class Single: Single
+		{
+			displayName= "Semi-Automatic";
+			textureType= "semi";
+			multiplier= "1"
+			aiDispersionCoefX = "1.4";
+			aiDispersionCoefY = "1.7";
+			aiRateOfFire = "2";
+			aiRateOfFireDispersion = "1";
+			aiRateOfFireDistance = "25";
+			artilleryCharge = "1";
+			artilleryDispersion = "1";
+			autoFire = "0";
+			burst = "1";
+			burstRangeMax = "-1";
+			canShootInWater = "0";
+			dispersion = "4.9999999e-005";
+			ffCount = "1";
+			ffFrequency = "11";
+			ffMagnitude = "0.5";
+			flash = "gunfire";
+			flashSize = "0.1";
+			maxRange = "50";
+			maxRangeProbab = "0.1";
+			midRange = "25";
+			midRangeProbab = "0.6";
+			minRange = "5";
+			minRangeProbab = "0.3";
+			recoil = "recoil_pistol_light";
+			recoilProne = "recoil_prone_pistol_light";
+			showToPlayer = "1";
+			soundBurst = "0";
+			soundContinuous = "0";
+			useAction = "0";
+			useActionTitle = "";
+			sounds[]=
+			{
+				"StandardSound",
+				"SilencedSound"
+			};
+			class BaseSoundModeType
+			{
+				weaponSoundEffect="DefaultRifle";
+				closure1[]={};
+				closure2[]={};
+				soundClosure[]=
+				{
+					"closure1",
+					0.5,
+					"closure2",
+					0.5
+				};
+			};
+			class StandardSound: BaseSoundModeType
+			{
+				soundSetShot[]=
+				{
+					"P07_Shot_SoundSet",
+					"P07_Tail_SoundSet",
+					"P07_InteriorTail_SoundSet"
+				};
+			};
+			class SilencedSound: BaseSoundModeType
+			{
+				SoundSetShot[]=
+				{
+					"P07_silencerShot_SoundSet",
+					"P07_silencerTail_SoundSet",
+					"P07_silencerInteriorTail_SoundSet"
+				};
+			};
+			reloadTime=0.1;
 		};
 	};
 };
