052c9e6fcac041820bd3ad6289a577f6d12b87d4
IsoBones <isoboness@gmail.com>
2023-06-01 19:47:11 +1000

    Update ASDG.hpp
    Removed LRCO optics

--- Addons/adfu_weapon_ef88/ASDG.hpp
+++ Addons/adfu_weapon_ef88/ASDG.hpp
@@ -10,8 +10,6 @@ class asdg_OpticRail1913: asdg_OpticRail
 {
     class compatibleItems
     {
-        optic_LRCO_blk_F = true;
-        optic_LRCO_snd_F = true;
     };
 };
 class asdg_OpticRail1913_short;
