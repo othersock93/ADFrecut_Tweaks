0e2ca904ddd064696cb3b506c37d651d192f5c2f
IsoBones <isoboness@gmail.com>
2023-06-06 20:42:33 +1000

    Update CfgPatches.hpp
    - Fixed "not an array" error

--- Addons/adfu_land_landrover/CfgPatches.hpp
+++ Addons/adfu_land_landrover/CfgPatches.hpp
@@ -2,14 +2,9 @@ class CfgPatches
 {
 	class ADFU_LandRover_4x4
 	{
-		units[]				= {"LandRover_Transport_ADFU"};
-		weapons[]			= {};
-		requiredVersion		= 0.1;
-		requiredAddons[]	= {"A3_Soft_F"};
-		author[] = {"Australian Defence Force: Uncut"};
-        authorUrl = "http://www.adf-uncut.com.au";
-        version = 0.4.5;
-        versionStr = "0.4.5";
-        versionAr[] = {0,4,5};
+		units[] = {"ADFU_LandRover_4x4"};
+		weapons[] = {};
+		requiredVersion	= 0.1;
+		requiredAddons[] = {"A3_Soft_F"};
 	};
 };
