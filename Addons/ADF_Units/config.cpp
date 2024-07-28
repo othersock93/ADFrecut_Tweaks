d6875913432716d23bc3fa4d33c96729d60d1a35
IsoBones <isoboness@gmail.com>
2024-07-28 10:06:24 +1000

    Manual merge branch #78 to main - ADF_UNITS

---
+++ Addons/ADF_Units/config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches
+{
+	class ADF_Units
+	{
+		name="ADF_Units";
+		author=$STR_ADF_AUTHOR;
+		requiredAddons[]=
+        	{
+			"A3_Data_F",
+			"ADF_Core"
+		};
+		units[]={};
+		weapons[]={};
+		fileName = "ADF_Units.pbo";
+	};
+};
