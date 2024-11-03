17aff07f363b69a7e876dc298a4ab7df1d8a4621
Brucey <spitfirebruce@gmail.com>
2024-11-03 19:09:05 +1000

    Tweaked HK417 Preset to use correct Hk417 (Black)

--- Addons/ADF_Weapons/adfrc_hk417/config.cpp
+++ Addons/ADF_Weapons/adfrc_hk417/config.cpp
@@ -7,54 +7,18 @@ class CfgPatches
 		units[] = {};
 		weapons[] = 
 		{
-			"arifle_SPAR_03_snd_F",
-			"ADFRC_hk417_base",
-			"ADFRC_hk417_sand",
-			"ADFRC_hk417_sand_army"
+			"arifle_SPAR_03_blk_F",
+			"ADFRC_hk417_DMR",
 		};
 	};
 };
 
 
-
-
-class asdg_OpticRail1913_long;
-class asdg_MuzzleSlot_762;
 class compatibleItems;
-class arifle_SPAR_03_snd_F;
+class arifle_SPAR_03_blk_F;
 class CfgWeapons
 {
-	class ADFRC_hk417_base: arifle_SPAR_03_snd_F
-	{
-		scope=1;
-		class WeaponSlotsInfo;
-	};
-	class ADFRC_hk417_sand: ADFRC_hk417_base
-	{
-		scope = 2;
-		baseWeapon= "HK417 (Sand)";
-		displayName = "HK417 (Sand)";
-		class WeaponSlotsInfo: WeaponSlotsInfo
-		{
-			allowedSlots = [901];
-			class CowsSlot: asdg_OpticRail1913_long 
-			{
-				class compatibleItems: compatibleItems 
-				{
-					ADFRC_acog_ta648_rds_t = 1;
-				};
-				displayName = "$STR_A3_CowsSlot0";
-				iconPicture = "\a3\weapons_f\Data\ui\attachment_top";
-				iconPinpoint = "Bottom";
-				iconPosition = [0.45,0.28];
-				iconScale = 0.2;
-				linkProxy = "\A3\data_f\proxies\weapon_slots\TOP";
-				scope = 0;
-			};
-			class MuzzleSlot: asdg_MuzzleSlot_762{};
-		};
-	};
-	class ADFRC_hk417_sand_army: ADFRC_hk417_sand
+	class ADFRC_hk417_DMR: arifle_SPAR_03_blk_F
 	{
 		scope=1;
 		author="$STR_ADFRC_AUTHOR";
@@ -63,7 +27,7 @@ class CfgWeapons
 			class LinkedItemsOptic
 			{
 				slot="CowsSlot";
-				item="ADFRC_acog_ta648_rds_t";
+				item="ADFRC_acog_ta648_rds";
 			};
 			class LinkedItemsPointer
 			{
@@ -73,7 +37,7 @@ class CfgWeapons
 			class LinkItemsUnder
 			{
 				slot = "UnderBarrelSlot";
-				item = "bipod_01_F_snd";
+				item = "ADFRC_Atlas_BT10";
 			};
 		};
 	};
