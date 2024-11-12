fa2f641bc95760a2baf335868f9adb88aa53f2e3
IsoBones <isoboness@gmail.com>
2024-11-12 17:03:49 +1100

    Added GWOT and PCW factions

--- Addons/ADF_Core/cfgFactionClasses.hpp
+++ Addons/ADF_Core/cfgFactionClasses.hpp
@@ -8,6 +8,22 @@ class cfgFactionClasses
 		priority=1;
 		side=1;
 	};
+	class ADFRC_F_GWOT
+	{
+		displayName="ADF (Global War on Terror)";
+		flag="\ADF_Core\data\ADF_flag_co.paa";
+		icon="\ADF_Core\data\ADF_icon_co.paa";
+		priority=1;
+		side=1;
+	};
+	class ADFRC_F_PCW
+	{
+		displayName="ADF (Post Cold-War)";
+		flag="\ADF_Core\data\ADF_flag_co.paa";
+		icon="\ADF_Core\data\ADF_icon_co.paa";
+		priority=1;
+		side=1;
+	};
 	////OLD CLASSES
 	class ADF
 	{
