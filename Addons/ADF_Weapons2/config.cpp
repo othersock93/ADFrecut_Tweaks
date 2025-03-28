cf14d51fce87a5d4d08670b96a36a0c9301e3b9d
Brucey <spitfirebruce@gmail.com>
2025-03-28 21:03:34 +1000

    Created Weapons2 For future updates

---
+++ Addons/ADF_Weapons2/config.cpp
@@ -0,0 +1,11 @@
+class CfgPatches
+{
+	class ADF_Weapons2
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"ADF_Core","ADF_Weapons"};
+		fileName = "ADF_Weapons2.pbo";
+	};
+};
