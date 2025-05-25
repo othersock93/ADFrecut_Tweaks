bb8c6bb0024fed489ad165e99d53a9a39d72ebe9
Brucey <spitfirebruce@gmail.com>
2025-05-25 16:44:34 +1000

    Updated TA31s to 1.0 standards

--- Addons/ADF_Optics/TA31/TA31.hpp
+++ Addons/ADF_Optics/TA31/TA31.hpp
@@ -8,34 +8,46 @@ class ADFRC_TA31_BLK : ItemCore
 		scope =2;
 		descriptionShort = "TA31 4x32";
 		
-		class ItemInfo: InventoryOpticsItem_Base_F
-		{
-			mass = 0.14;
-			modelOptics = "\A3\Weapons_F\empty";
-			optics = 1;
-			class OpticsModes
-			{
-				class Optic432
-				{
-					opticsID = 2;
-					useModelOptics = 0;
-					opticsZoomMin = 0.0625;
-					opticsZoomMax = 0.0625;
-					opticsZoomInit = 0.0625;
-					memoryPointCamera = "eye1";
-					opticsFlare = 1;
-					opticsDisablePeripherialVision = 1;
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\TA31\Textures\ADRC_ACOG_Ret_CA.paa";
+            reticleTextureSize = 0.33;
+
+            bodyTexture = "\ADF_Optics\TA31\Textures\ScopeBody_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\TA31\Textures\ScopeBody_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
+        weaponInfoType = "CBA_ScriptedOptic";
+		
+		class ItemInfo : InventoryOpticsItem_Base_F {
+			opticType = 2;
+			mass = 10;
+			RMBhint = "1/4x";
+			optics = true;
+			modelOptics = "\x\cba\addons\optics\cba_optic_small_pip.p3d";
+
+			class OpticsModes {
+				class Snip {
+					opticsID = 1;
+					opticsDisplayName = WFOV;
+					useModelOptics = 1;
+					opticsPPEffects[] = {"OpticsCHAbera1", "OpticsBlur1"};
+					opticsZoomMin = "4 call (uiNamespace getVariable 'cba_optics_fnc_setOpticMagnificationHelper')";
+					opticsZoomMax = "4 call (uiNamespace getVariable 'cba_optics_fnc_setOpticMagnificationHelper')";
+					opticsZoomInit = "4 call (uiNamespace getVariable 'cba_optics_fnc_setOpticMagnificationHelper')";
 					discreteDistance[] = {100};
-					discreteDistanceInitIndex = 2;
-					distanceZoomMin = 50;
-					distanceZoomMax = 50;
+					discreteDistanceInitIndex = "1 call (uiNamespace getVariable 'cba_optics_fnc_setOpticMagnificationHelperZeroing')";
+					distanceZoomMin = 100;
+					distanceZoomMax = 100;
+					memoryPointCamera = "opticView";
+					visionMode[] = {"Normal"};
+					opticsFlare = true;
+					opticsDisablePeripherialVision = true;
 					cameraDir = "";
-					visionMode[] = {};
-					opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1"};
 				};
 				class Irons
 				{
-					opticsID = 3;
+					opticsID = 2;
 					useModelOptics = 0;
 					opticsZoomMin = 0.25;
 					opticsZoomMax = 1.25;
@@ -52,7 +64,9 @@ class ADFRC_TA31_BLK : ItemCore
 					opticsPPEffects[] = {"Default"};
 				};
 			};
+
 		};
+		
 		
 		inertia = 0.12;
 	};
@@ -64,9 +78,18 @@ class ADFRC_TA31_FDE : ADFRC_TA31_BLK
 		picture="\ADF_Optics\TA31\UI\TA31_FDE_ca.paa";
 		uipicture="\ADF_Optics\TA31\UI\TA31_FDE_ca.paa";
 		model = "ADF_Optics\TA31\ADFRC_TA31_FDE.p3d";
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\TA31\Textures\ADRC_ACOG_Ret_CA.paa";
+            reticleTextureSize = 0.33;
+
+            bodyTexture = "\ADF_Optics\TA31\Textures\ScopeBody_FDE_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\TA31\Textures\ScopeBody_FDE_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 	};
 	
-class ADFRC_TA31_RMR_BLK : ItemCore
+class ADFRC_TA31_RMR_BLK : ADFRC_TA31_BLK
 	{
 		displayName="Trijicon ACOG TA31 4x32 RMR (Black)";
 		author="Brucey";
@@ -76,51 +99,14 @@ class ADFRC_TA31_RMR_BLK : ItemCore
 		scope =2;
 		descriptionShort = "TA31 4x32";
 		
-		class ItemInfo: InventoryOpticsItem_Base_F
-		{
-			mass = 0.14;
-			modelOptics = "\A3\Weapons_F\empty";
-			optics = 1;
-			class OpticsModes
-			{
-				class Optic432
-				{
-					opticsID = 2;
-					useModelOptics = 0;
-					opticsZoomMin = 0.0625;
-					opticsZoomMax = 0.0625;
-					opticsZoomInit = 0.0625;
-					memoryPointCamera = "eye1";
-					opticsFlare = 1;
-					opticsDisablePeripherialVision = 1;
-					discreteDistance[] = {100};
-					discreteDistanceInitIndex = 2;
-					distanceZoomMin = 50;
-					distanceZoomMax = 50;
-					cameraDir = "";
-					visionMode[] = {};
-					opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1"};
-				};
-				class Irons
-				{
-					opticsID = 3;
-					useModelOptics = 0;
-					opticsZoomMin = 0.25;
-					opticsZoomMax = 1.25;
-					opticsZoomInit = 0.75;
-					memoryPointCamera = "eye2";
-					opticsFlare = 0;
-					opticsDisablePeripherialVision = 0;
-					discreteDistance[] = {100};
-					discreteDistanceInitIndex = 2;
-					distanceZoomMin = 50;
-					distanceZoomMax = 50;
-					cameraDir = "";
-					visionMode[] = {};
-					opticsPPEffects[] = {"Default"};
-				};
-			};
-		};
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\TA31\Textures\ADRC_ACOG_Ret_CA.paa";
+            reticleTextureSize = 0.33;
+
+            bodyTexture = "\ADF_Optics\TA31\Textures\ScopeBody_RMR_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\TA31\Textures\ScopeBody_RMR_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 		
 		inertia = 0.15;
 	};
@@ -132,9 +118,18 @@ class ADFRC_TA31_RMR_FDE : ADFRC_TA31_RMR_BLK
 		picture="\ADF_Optics\TA31\UI\TA31_RMR_FDE_ca.paa";
 		uipicture="\ADF_Optics\TA31\UI\TA31_RMR_FDE_ca.paa";
 		model = "ADF_Optics\TA31\ADFRC_TA31_RMR_FDE.p3d";
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\TA31\Textures\ADRC_ACOG_Ret_CA.paa";
+            reticleTextureSize = 0.33;
+
+            bodyTexture = "\ADF_Optics\TA31\Textures\ScopeBody_FDE_RMR_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\TA31\Textures\ScopeBody_FDE_RMR_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 	};
 	
-class ADFRC_TA31_KF_BLK : ItemCore
+class ADFRC_TA31_KF_BLK : ADFRC_TA31_BLK
 	{
 		displayName="Trijicon ACOG TA31 4x32 (Black/KF)";
 		author="Brucey";
@@ -143,52 +138,6 @@ class ADFRC_TA31_KF_BLK : ItemCore
 		model = "ADF_Optics\TA31\ADFRC_TA31_KF_BLK.p3d";
 		scope =2;
 		descriptionShort = "TA31 4x32";
-		
-		class ItemInfo: InventoryOpticsItem_Base_F
-		{
-			mass = 0.14;
-			modelOptics = "\A3\Weapons_F\empty";
-			optics = 1;
-			class OpticsModes
-			{
-				class Optic432
-				{
-					opticsID = 2;
-					useModelOptics = 0;
-					opticsZoomMin = 0.0625;
-					opticsZoomMax = 0.0625;
-					opticsZoomInit = 0.0625;
-					memoryPointCamera = "eye1";
-					opticsFlare = 1;
-					opticsDisablePeripherialVision = 1;
-					discreteDistance[] = {100};
-					discreteDistanceInitIndex = 2;
-					distanceZoomMin = 50;
-					distanceZoomMax = 50;
-					cameraDir = "";
-					visionMode[] = {};
-					opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1"};
-				};
-				class Irons
-				{
-					opticsID = 3;
-					useModelOptics = 0;
-					opticsZoomMin = 0.25;
-					opticsZoomMax = 1.25;
-					opticsZoomInit = 0.75;
-					memoryPointCamera = "eye2";
-					opticsFlare = 0;
-					opticsDisablePeripherialVision = 0;
-					discreteDistance[] = {100};
-					discreteDistanceInitIndex = 2;
-					distanceZoomMin = 50;
-					distanceZoomMax = 50;
-					cameraDir = "";
-					visionMode[] = {};
-					opticsPPEffects[] = {"Default"};
-				};
-			};
-		};
 		
 		inertia = 0.13;
 	};
@@ -200,9 +149,18 @@ class ADFRC_TA31_KF_FDE : ADFRC_TA31_KF_BLK
 		picture="\ADF_Optics\TA31\UI\TA31_KF_FDE_ca.paa";
 		uipicture="\ADF_Optics\TA31\UI\TA31_KF_FDE_ca.paa";
 		model = "ADF_Optics\TA31\ADFRC_TA31_KF_FDE.p3d";
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\TA31\Textures\ADRC_ACOG_Ret_CA.paa";
+            reticleTextureSize = 0.33;
+
+            bodyTexture = "\ADF_Optics\TA31\Textures\ScopeBody_FDE_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\TA31\Textures\ScopeBody_FDE_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 	};
 	
-class ADFRC_TA31_KF_RMR_BLK : ItemCore
+class ADFRC_TA31_KF_RMR_BLK : ADFRC_TA31_BLK
 	{
 		displayName="Trijicon ACOG TA31 4x32 RMR (Black/KF)";
 		author="Brucey";
@@ -212,53 +170,16 @@ class ADFRC_TA31_KF_RMR_BLK : ItemCore
 		scope =2;
 		descriptionShort = "TA31 4x32";
 		
-		class ItemInfo: InventoryOpticsItem_Base_F
-		{
-			mass = 0.14;
-			modelOptics = "\A3\Weapons_F\empty";
-			optics = 1;
-			class OpticsModes
-			{
-				class Optic432
-				{
-					opticsID = 2;
-					useModelOptics = 0;
-					opticsZoomMin = 0.0625;
-					opticsZoomMax = 0.0625;
-					opticsZoomInit = 0.0625;
-					memoryPointCamera = "eye1";
-					opticsFlare = 1;
-					opticsDisablePeripherialVision = 1;
-					discreteDistance[] = {100};
-					discreteDistanceInitIndex = 2;
-					distanceZoomMin = 50;
-					distanceZoomMax = 50;
-					cameraDir = "";
-					visionMode[] = {};
-					opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1"};
-				};
-				class Irons
-				{
-					opticsID = 3;
-					useModelOptics = 0;
-					opticsZoomMin = 0.25;
-					opticsZoomMax = 1.25;
-					opticsZoomInit = 0.75;
-					memoryPointCamera = "eye2";
-					opticsFlare = 0;
-					opticsDisablePeripherialVision = 0;
-					discreteDistance[] = {100};
-					discreteDistanceInitIndex = 2;
-					distanceZoomMin = 50;
-					distanceZoomMax = 50;
-					cameraDir = "";
-					visionMode[] = {};
-					opticsPPEffects[] = {"Default"};
-				};
-			};
-		};
-		
 		inertia = 0.16;
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\TA31\Textures\ADRC_ACOG_Ret_CA.paa";
+            reticleTextureSize = 0.33;
+
+            bodyTexture = "\ADF_Optics\TA31\Textures\ScopeBody_RMR_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\TA31\Textures\ScopeBody_RMR_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 	};
 	
 class ADFRC_TA31_KF_RMR_FDE : ADFRC_TA31_KF_RMR_BLK
@@ -268,4 +189,13 @@ class ADFRC_TA31_KF_RMR_FDE : ADFRC_TA31_KF_RMR_BLK
 		picture="\ADF_Optics\TA31\UI\TA31_KF_RMR_FDE_ca.paa";
 		uipicture="\ADF_Optics\TA31\UI\TA31_KF_RMR_FDE_ca.paa";
 		model = "ADF_Optics\TA31\ADFRC_TA31_KF_RMR_FDE.p3d";
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\TA31\Textures\ADRC_ACOG_Ret_CA.paa";
+            reticleTextureSize = 0.33;
+
+            bodyTexture = "\ADF_Optics\TA31\Textures\ScopeBody_FDE_RMR_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\TA31\Textures\ScopeBody_FDE_RMR_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 	};
