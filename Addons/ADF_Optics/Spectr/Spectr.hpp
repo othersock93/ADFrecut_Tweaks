cf1a66bc288097d8aed6609f558c57e5e177642b
Brucey <spitfirebruce@gmail.com>
2025-05-25 16:45:27 +1000

    Implemented CBA PIP to Spectrs

--- Addons/ADF_Optics/Spectr/Spectr.hpp
+++ Addons/ADF_Optics/Spectr/Spectr.hpp
@@ -8,70 +8,85 @@ class ADFRC_Specter_RAR : ItemCore
 		scope =2;
 		descriptionShort = "Spectr DR 1x/4x";
 		
-		class ItemInfo: InventoryOpticsItem_Base_F
-		{
-			mass = 0.14;
-			modelOptics = "\A3\Weapons_F\empty";
-			optics = 1;
-			class OpticsModes
-			{
-				class Optic1
-				{
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\Spectr\Textures\ADRC_Spectr_Ret_CA.paa";
+            reticleTextureSize = 1.05;
+
+            bodyTexture = "\ADF_Optics\Spectr\Textures\ScopeBody_RAR_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\Spectr\Textures\ScopeBody_RAR_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
+        weaponInfoType = "CBA_ScriptedOptic";
+		
+		class ItemInfo : InventoryOpticsItem_Base_F {
+			opticType = 2;
+			mass = 14;
+			RMBhint = "1/4x";
+			optics = true;
+			modelOptics = "\x\cba\addons\optics\cba_optic_small_pip.p3d";
+
+			class OpticsModes {
+				
+				class Snip {
 					opticsID = 1;
-					useModelOptics = 0;
-					opticsZoomMin = 0.25;
-					opticsZoomMax = 0.6;
-					opticsZoomInit = 0.75;
-					memoryPointCamera = "eye1";
-					opticsFlare = 0;
-					opticsDisablePeripherialVision = 0;
+					opticsDisplayName = WFOV;
+					useModelOptics = 1;
+					opticsPPEffects[] = {"OpticsCHAbera1", "OpticsBlur1"};
+					opticsZoomMin = "4 call (uiNamespace getVariable 'cba_optics_fnc_setOpticMagnificationHelper')";
+					opticsZoomMax = "4 call (uiNamespace getVariable 'cba_optics_fnc_setOpticMagnificationHelper')";
+					opticsZoomInit = "4 call (uiNamespace getVariable 'cba_optics_fnc_setOpticMagnificationHelper')";
 					discreteDistance[] = {100};
-					discreteDistanceInitIndex = 2;
-					distanceZoomMin = 50;
-					distanceZoomMax = 50;
+					discreteDistanceInitIndex = "1 call (uiNamespace getVariable 'cba_optics_fnc_setOpticMagnificationHelperZeroing')";
+					distanceZoomMin = 100;
+					distanceZoomMax = 100;
+					memoryPointCamera = "opticView";
+					visionMode[] = {"Normal"};
+					opticsFlare = true;
+					opticsDisablePeripherialVision = true;
 					cameraDir = "";
-					visionMode[] = {};
-					opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1"};
 				};
-				class Optic4
+				class Optic1x
 				{
-					opticsID = 2;
-					useModelOptics = 0;
-					opticsZoomMin = 0.0625;
-					opticsZoomMax = 0.0625;
-					opticsZoomInit = 0.0625;
-					memoryPointCamera = "eye1";
-					opticsFlare = 1;
-					opticsDisablePeripherialVision = 1;
-					discreteDistance[] = {100};
-					discreteDistanceInitIndex = 2;
-					distanceZoomMin = 50;
-					distanceZoomMax = 50;
-					cameraDir = "";
-					visionMode[] = {};
-					opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1"};
+					opticsID=1;
+					useModelOptics=0;
+					opticsPPEffects[]=
+					{
+						""
+					};
+					opticsZoomMin=0.375;
+					opticsZoomMax=1.1;
+					opticsZoomInit=0.75;
+					memoryPointCamera="eye1";
+					visionMode[]={};
+					opticsFlare=0;
+					opticsDisablePeripherialVision=0;
+					distanceZoomMin=300;
+					distanceZoomMax=300;
+					cameraDir="";
 				};
-				class Irons
+				class RDS1x
 				{
-					opticsID = 3;
-					useModelOptics = 0;
-					opticsZoomMin = 0.25;
-					opticsZoomMax = 1.25;
-					opticsZoomInit = 0.75;
-					memoryPointCamera = "eye2";
-					opticsFlare = 0;
-					opticsDisablePeripherialVision = 0;
-					discreteDistance[] = {100};
-					discreteDistanceInitIndex = 2;
-					distanceZoomMin = 50;
-					distanceZoomMax = 50;
-					cameraDir = "";
-					visionMode[] = {};
-					opticsPPEffects[] = {"Default"};
+					opticsID=1;
+					useModelOptics=0;
+					opticsPPEffects[]=
+					{
+						""
+					};
+					opticsZoomMin=0.375;
+					opticsZoomMax=1.1;
+					opticsZoomInit=0.75;
+					memoryPointCamera="eye2";
+					visionMode[]={};
+					opticsFlare=0;
+					opticsDisablePeripherialVision=0;
+					distanceZoomMin=300;
+					distanceZoomMax=300;
+					cameraDir="";
 				};
 			};
+
 		};
-		
 		inertia = 0.1;
 	};
 	
@@ -92,6 +107,15 @@ class ADFRC_Specter_CDO : ADFRC_Specter_RAR
 		picture="\ADF_Optics\Spectr\UI\Spectr_CDO_ca.paa";
 		uipicture="\ADF_Optics\Spectr\UI\Spectr_CDO_ca.paa";
 		model = "ADF_Optics\Spectr\ADFRC_Spectr_CDO.p3d";
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\Spectr\Textures\ADRC_Spectr_Ret_CA.paa";
+            reticleTextureSize = 1.05;
+
+            bodyTexture = "\ADF_Optics\Spectr\Textures\ScopeBody_CDO_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\Spectr\Textures\ScopeBody_CDO_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 	};
 	
 class ADFRC_Specter_CDO_KF : ADFRC_Specter_CDO
@@ -110,6 +134,15 @@ class ADFRC_Specter_CDO_DPP : ADFRC_Specter_RAR
 		picture="\ADF_Optics\Spectr\UI\Spectr_CDO_ca.paa";
 		uipicture="\ADF_Optics\Spectr\UI\Spectr_CDO_ca.paa";
 		model = "ADF_Optics\Spectr\ADFRC_Spectr_CDO_DP.p3d";
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\Spectr\Textures\ADRC_Spectr_Ret_CA.paa";
+            reticleTextureSize = 1.05;
+
+            bodyTexture = "\ADF_Optics\Spectr\Textures\ScopeBody_CDO_DPP_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\Spectr\Textures\ScopeBody_CDO_DPP_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 	};
 	
 class ADFRC_Specter_CDO_KF_DPP : ADFRC_Specter_CDO
@@ -119,6 +152,15 @@ class ADFRC_Specter_CDO_KF_DPP : ADFRC_Specter_CDO
 		picture="\ADF_Optics\Spectr\UI\Spectr_CDO_KF_ca.paa";
 		uipicture="\ADF_Optics\Spectr\UI\Spectr_CDO_KF_ca.paa";
 		model = "ADF_Optics\Spectr\ADFRC_Spectr_CDO_DP_KF.p3d";
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\Spectr\Textures\ADRC_Spectr_Ret_CA.paa";
+            reticleTextureSize = 1.05;
+
+            bodyTexture = "\ADF_Optics\Spectr\Textures\ScopeBody_CDO_DPP_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\Spectr\Textures\ScopeBody_CDO_DPP_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 	};
 	
 	
@@ -128,10 +170,20 @@ class ADFRC_Specter_C_RAR : ADFRC_Specter_RAR
 		picture="\ADF_Optics\Spectr\UI\Spectr_RAR_C_ca.paa";
 		uipicture="\ADF_Optics\Spectr\UI\Spectr_RAR_C_ca.paa";
 		model = "ADF_Optics\Spectr\ADFRC_Spectr_C_RAR.p3d";
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\Spectr\Textures\ADRC_Spectr_Ret_CA.paa";
+            reticleTextureSize = 1.05;
+
+            bodyTexture = "\ADF_Optics\Spectr\Textures\ScopeBody_RAR_C_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\Spectr\Textures\ScopeBody_RAR_C_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 	};
 	
 class ADFRC_Specter_RAR_C : ADFRC_Specter_C_RAR
 	{
+	scope = 1;
 	};
 	
 class ADFRC_Specter_RAR_C_KF : ADFRC_Specter_RAR
@@ -141,6 +193,15 @@ class ADFRC_Specter_RAR_C_KF : ADFRC_Specter_RAR
 		picture="\ADF_Optics\Spectr\UI\Spectr_RAR_C_KF_ca.paa";
 		uipicture="\ADF_Optics\Spectr\UI\Spectr_RAR_C_KF_ca.paa";
 		model = "ADF_Optics\Spectr\ADFRC_Spectr_C_RAR_KF.p3d";
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\Spectr\Textures\ADRC_Spectr_Ret_CA.paa";
+            reticleTextureSize = 1.05;
+
+            bodyTexture = "\ADF_Optics\Spectr\Textures\ScopeBody_RAR_C_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\Spectr\Textures\ScopeBody_RAR_C_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 	};
 	
 
@@ -152,6 +213,15 @@ class ADFRC_Specter_C_CDO : ADFRC_Specter_RAR
 		picture="\ADF_Optics\Spectr\UI\Spectr_CDO_C_ca.paa";
 		uipicture="\ADF_Optics\Spectr\UI\Spectr_CDO_C_ca.paa";
 		model = "ADF_Optics\Spectr\ADFRC_Spectr_C_CDO.p3d";
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\Spectr\Textures\ADRC_Spectr_Ret_CA.paa";
+            reticleTextureSize = 1.05;
+
+            bodyTexture = "\ADF_Optics\Spectr\Textures\ScopeBody_C_CDO_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\Spectr\Textures\ScopeBody_C_CDO_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 	};
 	
 class ADFRC_Specter_CDO_C_KF : ADFRC_Specter_CDO
@@ -161,6 +231,15 @@ class ADFRC_Specter_CDO_C_KF : ADFRC_Specter_CDO
 		picture="\ADF_Optics\Spectr\UI\Spectr_CDO_C_KF_ca.paa";
 		uipicture="\ADF_Optics\Spectr\UI\Spectr_CDO_C_KF_ca.paa";
 		model = "ADF_Optics\Spectr\ADFRC_Spectr_C_CDO_KF.p3d";
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\Spectr\Textures\ADRC_Spectr_Ret_CA.paa";
+            reticleTextureSize = 1.05;
+
+            bodyTexture = "\ADF_Optics\Spectr\Textures\ScopeBody_C_CDO_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\Spectr\Textures\ScopeBody_C_CDO_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 	};
 	
 class ADFRC_Specter_CDO_C_DPP : ADFRC_Specter_RAR
@@ -170,6 +249,15 @@ class ADFRC_Specter_CDO_C_DPP : ADFRC_Specter_RAR
 		picture="\ADF_Optics\Spectr\UI\Spectr_CDO_c_ca.paa";
 		uipicture="\ADF_Optics\Spectr\UI\Spectr_CDO_c_ca.paa";
 		model = "ADF_Optics\Spectr\ADFRC_Spectr_C_CDO_DP.p3d";
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\Spectr\Textures\ADRC_Spectr_Ret_CA.paa";
+            reticleTextureSize = 1.05;
+
+            bodyTexture = "\ADF_Optics\Spectr\Textures\ScopeBody_C_CDO_DPP_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\Spectr\Textures\ScopeBody_C_CDO_DPP_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 	};
 	
 class ADFRC_Specter_CDO_C_KF_DPP : ADFRC_Specter_CDO
@@ -179,5 +267,14 @@ class ADFRC_Specter_CDO_C_KF_DPP : ADFRC_Specter_CDO
 		picture="\ADF_Optics\Spectr\UI\Spectr_CDO_c_KF_ca.paa";
 		uipicture="\ADF_Optics\Spectr\UI\Spectr_CDO_c_KF_ca.paa";
 		model = "ADF_Optics\Spectr\ADFRC_Spectr_C_CDO_DP_KF.p3d";
+		
+		class CBA_ScriptedOptic {
+            reticleTexture = "\ADF_Optics\Spectr\Textures\ADRC_Spectr_Ret_CA.paa";
+            reticleTextureSize = 1.05;
+
+            bodyTexture = "\ADF_Optics\Spectr\Textures\ScopeBody_C_CDO_DPP_D_ca.paa";
+            bodyTextureNight = "\ADF_Optics\Spectr\Textures\ScopeBody_C_CDO_DPP_N_ca.paa";
+			bodyTextureSize = 1.15;
+        };
 	};
 	
